0c32f2a16cc6a52c457df7d56157dce4ef3aaf3a
Dawid Ryskala <rdawid296@gmail.com>
2023-10-12 23:52:23 +0200

    Almost finished

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import Stats from "./Stats";
+import PanckingList from "./PanckingList";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    console.log(id);
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div>
+      <Logo />
+      <Form onAddItems={handleItems} />
+      <PanckingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,9 @@
-import React, { useState } from "react";
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
-export default function App() {
-  return (
-    <div>
-      <Logo />
-      <Form />
-      <PanckingList />
-      <Stats />
-    </div>
-  );
-}
-
-function Logo() {
-  return <h1>🌴 Far away 🧳</h1>;
-}
+import { useState } from "react";
 
 //in React we usually do not use event to take data.
 // While we using Controlled Elements
-function Form() {
+
+export default function Form({ onAddItems }) {
   const [description, setDescription] = useState("Test");
   const [quantity, setQuantity] = useState(1);
 
@@ -36,7 +16,10 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -64,37 +47,5 @@ function Form() {
       />
       <button>Add</button>
     </form>
-  );
-}
-
-// destructering props.item --> {item}
-function Item({ item }) {
-  return (
-    <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button>❌</button>
-    </li>
-  );
-}
-
-function PanckingList() {
-  return (
-    <div className="list">
-      <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function Stats() {
-  return (
-    <footer className="stats">
-      <em> 💼 You have X items on your list, and you already packed X (X%)</em>
-    </footer>
   );
 }
